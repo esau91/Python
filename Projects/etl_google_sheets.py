57ebf3d911ee80582e5f215a32758b4b488fdc65
Esau Reyes <esau.gk.91@gmail.com>
2021-02-09 18:09:43 -0600

    Dictionary configuration

--- Projects/etl_google_sheets.py
+++ Projects/etl_google_sheets.py
@@ -18,7 +18,7 @@ def get_auth(secrets_file):
         logging.info('Authentication succedd')
 
     except Exception as e:
-        logging.error('The following error happend when trying to authenticate: {}'.format(e))
+        logging.exception('The following error happend when trying to authenticate: ')
         exit(1)
 
     return client
@@ -32,7 +32,7 @@ def get_sheets_file(client, file_name, sheet_name, skip_rows= 0): #, secrets_fil
         logging.info("The file '{}' was successfully accessed".format(file_name))
 
     except Exception as e:
-        logging.error('The following error happend when trying to access the file: {}'.format(e))
+        logging.exception('The following error happend when trying to access the file: ')
         exit(1)
     
     raw_data = pd.DataFrame(sheet.get_all_values()[skip_rows:])
@@ -65,25 +65,50 @@ def write_to_sheets(client, file_name, sheet_name, input_df):
     try:
         set_with_dataframe(sheet, input_df, include_column_header=True, row= current_rows_len + 1)
     except Exception as e:
-        logger.error('The following error happend while trying to write: {}'.format(e))
+        logging.exception('The following error happend while trying to write: ')
         exit(1)
 
 
 def main():
-    secrets_file = '.json' #path to you secrets file
-    ads_file_name = 'Google Ads Plataforma'
-    ads_sheet_name = 'Google Ads Plataforma'
-    ads_pfm_columns = ['Campaña','Grupo de anuncios','Día','Moneda','Clics','Impresiones','Costo','Video reproducido al 100%']
-    ads_pfm_date = ['Día']
-    ads_pfm_skip_rows = 2
-    ads_pfm_output_file_name = 'Master Welchs'
-    ads_pfm_output_sheet_name= 'Google_ads_pfm'
+
+    logging.basicConfig(level=logging.DEBUG)
+    secrets_file = '' #path to secrets file
+    output_file_name = 'Master Welchs'
+
+    config_values = {
+        'Analytics': {
+            'file_name': 'Analytics',
+            'sheet_name': 'Raw',
+            'columns': ['Campaña', 'Contenido del anuncio', 'Fecha ga', 'INGRESOS'],
+            'columns_to_date': ['Fecha ga'],
+            'skip_rows': 0,
+            'output_sheet_name': 'Analytics'
+            },
+        'Google Ads Plataforma': {
+            'file_name': 'Google Ads Plataforma',
+            'sheet_name': 'Google Ads Plataforma',
+            'columns': ['Campaña','Grupo de anuncios','Día','Moneda','Clics','Impresiones','Costo','Video reproducido al 100%'],
+            'columns_to_date': ['Día'],
+            'skip_rows': 2,
+            'output_sheet_name': 'Google_ads_pfm'
+            },
+        'Facebook': {
+            'file_name': 'Facebook',
+            'sheet_name': 'Raw',
+            'columns': ['Nombre de la campaña', 'Nombre del conjunto de anuncios', 'Nombre del anuncio', 'Día', 'Divisa', 'Clics en el enlace', 'Impresiones', 'Inversion', 'Reproducciones de video hasta el 100%'],
+            'columns_to_date': ['Día'],
+            'skip_rows': 0,
+            'output_sheet_name': 'Facebook'
+            }
+    }
+    
+    input_source = 'Analytics'
 
     client = get_auth(secrets_file)
-    raw_df = get_sheets_file(client, ads_file_name, ads_sheet_name, ads_pfm_skip_rows)
-    formatted_df = output_format(raw_df, ads_pfm_columns, ads_pfm_date)
-    write_to_sheets(client, ads_pfm_output_file_name, ads_pfm_output_sheet_name, formatted_df)
-    logger.info('Successful execution')
+    raw_df = get_sheets_file(client, config_values[input_source]['file_name'],  config_values[input_source]['sheet_name'], config_values[input_source]['skip_rows'])
+    formatted_df = output_format(raw_df, config_values[input_source]['columns'], config_values[input_source]['columns_to_date'])
+    write_to_sheets(client, output_file_name, config_values[input_source]['output_sheet_name'], formatted_df)
+    logging.info('Successful execution')
 
 if __name__ == '__main__':
     main()
