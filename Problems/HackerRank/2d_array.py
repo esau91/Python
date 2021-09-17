9427b50d02fc4aa19ddb3fc794e1537304b8bdcd
esau91 <esau.gk.91@gmail.com>
2021-09-17 09:56:55 -0500

    partial sum

---
+++ Problems/HackerRank/2d_array.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'hourglassSum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def hourglassSum(arr):
+    # Write your code here
+    results = []
+    for i in range(4):
+        for j in range(4):
+            top = sum(arr[i][j:j+3])
+            middle = arr[i+1][j+1]
+            bottom = sum(arr[i+2][j:j+3])
+            results.append(sum([top, middle, bottom]))
+            
+    print(results)
+    return max(results)
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
