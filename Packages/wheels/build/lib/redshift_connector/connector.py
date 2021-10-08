72bcbd95fb9bcbc9d4c4294973076ea74087e707
esau91 <esau.gk.91@gmail.com>
2021-10-08 13:56:23 -0500

    New wheel version, adds support to glue connections

--- Packages/wheels/build/lib/redshift_connector/connector.py
+++ Packages/wheels/build/lib/redshift_connector/connector.py
@@ -4,7 +4,6 @@ import pg8000.dbapi
 from botocore.exceptions import ClientError
 
 def get_secret(secret_name, region_name):
-
     session = boto3.session.Session()
     client = session.client(
         service_name='secretsmanager',
@@ -20,27 +19,59 @@ def get_secret(secret_name, region_name):
     except ClientError as e:
         raise e
 
-def connect_to_redshift(database_name, secret_name = None, region_name = None, secret = None):
-    '''
-    secret = {
-        'username': '',
-        'password': '',
-        'port': '',
-        'host': ''
-    }
-    '''
-    if database_name is None:
-        return 'the parameter database_name is mandatory'
+def get_connection_values(connection_name):
+    #connnection_name must have the next structure: 'jdbc:redshift://###############.redshift.amazonaws.com:PORT/DB_NAME
+    client = boto3.client('glue')
+    my_connection_params = {}
+    try:
+        glue_connection = client.get_connection(Name=connection_name)
+        properties = glue_connection['Connection']['ConnectionProperties']
+        if properties:
+            jbdc_url = properties['JDBC_CONNECTION_URL'].replace('//','').split(':')
+            my_connection_params['host'] = jbdc_url[2]
+            my_connection_params['database'] = jbdc_url[-1].split('/')[1]
+            my_connection_params['port'] = jbdc_url[-1].split('/')[0]
+            my_connection_params['username'] = properties['USERNAME']
+            my_connection_params['password'] = properties['PASSWORD']
 
-    if secret is None:
-        secret = get_secret(secret_name, region_name)   
+        return my_connection_params
+    except Exception as e:
+        raise e
+
+
+
+def connect_to_redshift(secret_dict = None, glue_connection = None, secret_name = None, region_name = None):
+
+    if secret_dict is None and glue_connection is None and secret_name is None or\
+    (secret_name and region_name is None):
+        print( """You must pass one of the following parameters:
+                1. A dictionary defined by you with the following key/values
+                secret_dict = {
+                    'username': '',
+                    'password': '',
+                    'database:' '',
+                    'port': '',
+                    'host': ''}
+                2. Or a name of the glue_connection with the same value/keys as #1
+                3. Or a name of the secret with the same value/keys as #1 and region where is located""")
+        return 1
+                
+            
+    #Testing without Secrets Manager
+    if secret_dict:
+        secret_values = secret_dict
+    #Using Glue Connections
+    elif glue_connection:
+        secret_values = glue_connection
+    #Using Secrets Manager
+    else:
+        secret_values = get_secret(secret_name, region_name)   
     
-    REDSHIFT_USER = secret['username']
-    REDSHIFT_PASSWORD = secret['password']
-    REDSHIFT_DATABASE = database_name
-    REDSHIFT_CLUSTER = secret['dbClusterIdentifier']
-    REDSHIFT_PORT = secret['port']
-    REDSHIFT_ENDPOINT = secret['host']
+    REDSHIFT_USER = secret_values['username']
+    REDSHIFT_PASSWORD = secret_values['password']
+    REDSHIFT_DATABASE = secret_values['database']
+    REDSHIFT_PORT = secret_values['port']
+    REDSHIFT_ENDPOINT = secret_values['host']
 
     try:
         connection = pg8000.dbapi.connect(
--- Packages/wheels/setup.py
+++ Packages/wheels/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name = 'redshift_connector',
-        version = '1.1',
+        version = '1.2',
         author = 'Esaú Reyes',
         packages = ['redshift_connector'], #pfind_packages()
         install_requires=['scramp']
